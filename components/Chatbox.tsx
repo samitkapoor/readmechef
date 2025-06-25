77c0cf2ba5138cd522b2dd822edd8dcf924c15ee
samitkapoor <samitkapoor77@gmail.com>
2025-06-26 00:41:56 +0530

    Enhance Chatbox component for responsive markdown rendering
    - Updated Chatbox to conditionally render markdown based on screen size.
    - Modified extractExplanatoryText function to accept a parameter for mobile view handling, improving content extraction for different devices.

--- components/Chatbox.tsx
+++ components/Chatbox.tsx
@@ -104,10 +104,16 @@ const Chatbox = ({
                 </div>
               ) : (
                 <>
-                  <div className="text-white group rounded-2xl rounded-tl-sm text-xs p-6 md:text-sm w-full overflow-x-auto bg-[#151515] scrollbar-hide border-[1px] border-white/20 shadow-md max-w-[85%] tracking-tight relative">
+                  <div className="hidden lg:block text-white group rounded-2xl rounded-tl-sm text-xs p-6 md:text-sm w-full overflow-x-auto bg-[#151515] scrollbar-hide border-[1px] border-white/20 shadow-md max-w-[85%] tracking-tight relative">
                     <CopyButton text={message.display} />
 
-                    <RenderMarkdown markdown={extractExplanatoryText(message.display)} />
+                    <RenderMarkdown markdown={extractExplanatoryText(message.display, false)} />
+                  </div>
+
+                  <div className="block lg:hidden text-white group rounded-2xl rounded-tl-sm text-xs p-6 md:text-sm w-full overflow-x-auto bg-[#151515] scrollbar-hide border-[1px] border-white/20 shadow-md max-w-[85%] tracking-tight relative">
+                    <CopyButton text={message.display} />
+
+                    <RenderMarkdown markdown={extractExplanatoryText(message.display, true)} />
                   </div>
                 </>
               )}
